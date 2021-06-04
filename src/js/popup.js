96767d10848ff58507267435150495419cd4c895
slenane <slenane1@gmail.com>
2021-06-05 00:41:50 +0100

    Finished control panel and added random goal

--- src/js/popup.js
+++ src/js/popup.js
@@ -4,6 +4,8 @@ const chessAPI = new ChessWebAPI();
 import Chess from 'chess.js';
 
 let game, dailyPuzzle, board, correctMoves, correctFENs, totalMoves, moveCount, moveCountUI;
+let playing = false;
+let hintActive = false;
 
 let resetVariables = () => {
   game = undefined;
@@ -18,6 +20,9 @@ let resetVariables = () => {
   puzzleMoves.textContent = "";
 }
 
+// Puzzle type
+let puzzleType = document.querySelector('.random-slider');
+let nextPuzzleBtn = document.querySelector('.next-puzzle-button');
 // UI
 let statusBar = document.querySelector('.status-bar');
 let playerStatus = document.querySelector('.status');
@@ -152,9 +157,9 @@ let onDrop = (source, target) => {
   } else if (game.fen() === correctFENs[moveCount]) {
     // Display the move under the puzzle title
     if (game.turn() === 'b') {
-      puzzleMoves.insertAdjacentHTML('beforeend', `<span class='move-number'>${moveCountUI}.</span> ${correctMoves[moveCount]}`);
+      puzzleMoves.insertAdjacentHTML('beforeend', `<span class='move-number'>${moveCountUI}.</span> <span class="move move-${moveCount}">${correctMoves[moveCount]}</span>`);
     } else {
-      puzzleMoves.insertAdjacentHTML('beforeend', ` ${correctMoves[moveCount]} `);
+      puzzleMoves.insertAdjacentHTML('beforeend', ` <span class="move move-${moveCount}">${correctMoves[moveCount]}</span> `);
     }
     // Incremnet the UI move counter after user move
     moveCountUI++;
@@ -176,24 +181,28 @@ let onDrop = (source, target) => {
         updateStatus('clear');
         // Automate opponent moves
         makeNextMove();
-      }, 1000);
+      }, 750);
     }
   }
 }
 
 //  make opponent's move automatically or user's move through hint
 let makeNextMove = (hint) => {
+  // If there is a hint already active then return
+  if (hintActive === true && hint) return;
   // Clear the moves if the solution is already shown
   if (moveCount === 0) puzzleMoves.textContent = "";
   // If there is no valid move - return
   if (correctFENs[moveCount] === undefined) return;
+  // if it is a hint - activate hintActive
+  hintActive = true;
   // Display next move under the title
   if (hint && game.turn() === 'w') {
-    puzzleMoves.insertAdjacentHTML('beforeend', `<span class='move-number'>${moveCountUI}.</span> ${correctMoves[moveCount]}`);
+    puzzleMoves.insertAdjacentHTML('beforeend', `<span class='move-number'>${moveCountUI}.</span> <span class="move move-${moveCount}">${correctMoves[moveCount]}</span>`);
     // Incremnet the UI move counter after user move
     moveCountUI++;
   } else {
-    puzzleMoves.insertAdjacentHTML('beforeend', ` ${correctMoves[moveCount]} `);
+    puzzleMoves.insertAdjacentHTML('beforeend', ` <span class="move move-${moveCount}">${correctMoves[moveCount]}</span> `);
   }
   // Set the game to the FEN of the next move
   game.load(correctFENs[moveCount]);
@@ -208,17 +217,50 @@ let makeNextMove = (hint) => {
   if (moveCount === totalMoves) {
     // Update the UI under the board
     updateStatus('solved');
+    hintActive = false;
     return;
   } 
   // Make the next move for the opponent
   if (hint) {
-    setTimeout(makeNextMove, 1000);
+    setTimeout(makeNextMove, 750);
+  } else {
+    hintActive = false;
   }
 }
   
 // update the board position after the piece snap for castling, en passant, pawn promotion
 let onSnapEnd = () => {
   board.position(game.fen());
+}
+
+let changePuzzleType = async () => {
+  if (puzzleType.checked) {
+    nextPuzzleBtn.textContent = "18";
+    nextPuzzleBtn.classList.remove('hide');
+    await nextPuzzle()
+  } else {
+    await init();
+    nextPuzzleBtn.classList.add('hide');
+  }
+}
+
+let nextPuzzle = async () => {
+  // Start the next random puzzle
+  await init("random");
+  // Disable button click
+  nextPuzzleBtn.disabled = true;
+  let count = 17;
+  // Set countdown timer on button
+  let countdown = setInterval(() => {
+    if (count === 0) {
+      nextPuzzleBtn.disabled = false;
+      nextPuzzleBtn.textContent = "Next"
+      clearInterval(countdown);
+    } else {
+      nextPuzzleBtn.textContent = count;
+      count--;
+    }
+  }, 1000);
 }
 
 // Show the solution to the puzzle under the board
@@ -250,21 +292,96 @@ let showSolution = () => {
 }
 
 let playPuzzle = async () => {
-  await init();
-  showSolution();
-  updateStatus('solved');
-
-  // ADD PAUSE
-  let play = setInterval(() => {
-    if (moveCount === totalMoves - 1) clearInterval(play)
+  if (!playing) {
     // Set the game to the FEN of the next move
-    game.load(correctFENs[moveCount]);
+    game.load(dailyPuzzle.body.fen);
     // Update the board to show the new position
     board.position(game.fen());
     // Increment the counter to track the current move
-    moveCount++;
-  }, 1000);
+    moveCount = 0; 
+    playing = true;
+  
+    let play = setInterval(() => {
+      // If this is the last clear the interval
+      if (moveCount === totalMoves - 1) {
+        clearInterval(play);
+        playing = false;
+      }
+      // Set the game to the FEN of the next move
+      game.load(correctFENs[moveCount]);
+      // Update the board to show the new position
+      board.position(game.fen());
+      // Increment the counter to track the current move
+      moveCount++;
+    }, 750);
+  } else {
+    // If the puzzle is already playing then return
+    return;
+  }
 }  
+
+let getStartPosition = async () => {
+  // Set the game to the FEN of the next move
+  game.load(dailyPuzzle.body.fen);
+  // Update the board to show the new position
+  board.position(game.fen());
+  // Increment the counter to track the current move
+  moveCount = 0; 
+}
+
+let getPreviousPosition = async () => {
+  if (moveCount === 0) return;
+  let currentMove = moveCount - 1;
+  let previousMove = currentMove - 1;
+
+  if (previousMove < 0) {
+    // Set the game to the FEN of the next move
+    game.load(dailyPuzzle.body.fen);
+    // Update the board to show the new position
+    board.position(game.fen());
+    // Increment the counter to track the current move
+    moveCount--; 
+    return;
+  }
+
+  // Set the game to the FEN of the next move
+  game.load(correctFENs[previousMove]);
+  // Update the board to show the new position
+  board.position(game.fen());
+  // Increment the counter to track the current move
+  moveCount--;
+}
+
+let getNextPosition = async () => {
+  if (moveCount === totalMoves) return;
+  // Set the game to the FEN of the next move
+  game.load(correctFENs[moveCount]);
+  // Update the board to show the new position
+  board.position(game.fen());
+  // Increment the counter to track the current move
+  moveCount++;
+}
+
+let getLastPosition = async () => {
+  // Set move count to total moves
+  moveCount = totalMoves - 1;
+  // Set the game to the FEN of the next move
+  game.load(correctFENs[moveCount]);
+  // Update the board to show the new position
+  board.position(game.fen());
+}
+
+let selectMove = (e) => {
+  if (e.target.classList[0] !== 'move') return;
+  // Update the movecount to the move number in the class list of the target
+  moveCount = e.target.classList[1].split("-")[1];
+  console.log(moveCount);
+  // Set the game to the FEN of the next move
+  game.load(correctFENs[moveCount]);
+  // Update the board to show the new position
+  board.position(game.fen());
+};
+
 
 // chessboardjs
 // Display chessbaord and update if move is correct
@@ -284,11 +401,12 @@ let startGame = (puzzle) => {
   game = new Chess(puzzle.body.fen);
 };
 
-let init = async () => {
+let init = async (random) => {
   // Set initial values of all variables
   resetVariables();
   // Get the daily puzzle informaiton from chess.com
-  dailyPuzzle = await chessAPI.getDailyPuzzle();
+  if (random) dailyPuzzle = await chessAPI.getDailyPuzzleRandom();
+  else dailyPuzzle = await chessAPI.getDailyPuzzle();
   console.log(dailyPuzzle);
   // Display the puzzle informationon the screen
   displayInfo(dailyPuzzle);
@@ -302,26 +420,32 @@ let init = async () => {
   updateStatus("start");
 };
 
-// Puzzle controls
+// Random Puzzle Controls
+puzzleType.addEventListener('click', changePuzzleType);
+// Next puzzle
+nextPuzzleBtn.addEventListener('click', nextPuzzle);
+
+// Control Panel
 // Give hint
 hintIcon.addEventListener('click', () => makeNextMove("hint"));
 // Show solution
 solutionIcon.addEventListener('click', showSolution);
 // Reset game
-resetIcon.addEventListener('click', init);
+resetIcon.addEventListener('click', () => init());
 // Flip board
-flipIcon.addEventListener('click', () => board.flip())
+flipIcon.addEventListener('click', () => board.flip());
 // Play / Pause game
-playIcon.addEventListener('click', playPuzzle)
+playIcon.addEventListener('click', playPuzzle);
 // First move
-// startIcon.addEventListener('click', )
+startIcon.addEventListener('click', getStartPosition);
 // Previous move
-// backIcon.addEventListener('click', )
+backIcon.addEventListener('click', getPreviousPosition);
 // Next move
-// forwardIcon.addEventListener('click', )
+forwardIcon.addEventListener('click', getNextPosition);
 // Last move
-// endIcon
+endIcon.addEventListener('click', getLastPosition);
+// Select move
+puzzleMoves.addEventListener('click', (e) => selectMove(e));
 
-
-// Start the game!
+// Start the puzzle!
 init();
